ed4f115d12c177a4e47c64696b9ac224ab32540a
ohseongbin <dannybin814@naver.com>
2025-02-10 23:25:46 +0900

    Feat: Lecture domain

---
+++ src/main/java/gdsc/codereview/domain/lecture/domain/Lecture.java
@@ -0,0 +1,28 @@
+package gdsc.codereview.domain.lecture.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lecture_id")
+    private Long id;
+
+    // fk instruction id
+
+    private String title;
+
+    private String summary; //간단한 소개
+
+    private String content; //전체 내용
+
+    private String platform;
+
+    @Column(name = "lecture_score")
+    private Float score;
+
+}
