f7699d4949bd74d53e148ec0e28771202c38f73b
ohseongbin <dannybin814@naver.com>
2025-02-13 00:33:51 +0900

    Feat: category enum에 각 링크 추가

--- src/main/java/gdsc/codereview/domain/lecture/entity/Category.java
+++ src/main/java/gdsc/codereview/domain/lecture/entity/Category.java
@@ -4,15 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum Category {
-    WEB("Web Development"),
-    MOBILE("Mobile Development"),
-    PL("Programming Language"),
-    DB("Database"),
-    TEST("Software Testing");
+    WEB("Web Development","https://www.inflearn.com/courses/it-programming/web-dev?sort=RECENT"),
+    MOBILE("Mobile Development","https://www.inflearn.com/courses/it-programming/mobile-app?sort=RECENT"),
+    PL("Programming Language","https://www.inflearn.com/courses/it-programming/programming-lang?sort=RECENT"),
+    DB("Database","https://www.inflearn.com/courses/it-programming/database-dev?sort=RECENT"),
+    TEST("Software Testing","https://www.inflearn.com/courses/it-programming/sw-test?sort=RECENT");
 
     private final String displayName;
+    private final String url;
 
-    Category(String displayName){
+    Category(String displayName,String url){
         this.displayName = displayName;
+        this.url = url;
     }
 }
