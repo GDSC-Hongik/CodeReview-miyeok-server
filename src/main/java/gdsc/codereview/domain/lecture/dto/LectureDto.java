c625a31e351ad67a8745088fe244398344b59546
ohseongbin <dannybin814@naver.com>
2025-02-12 21:36:47 +0900

    Refactor: Builder 패턴 도입

--- src/main/java/gdsc/codereview/domain/lecture/dto/LectureDto.java
+++ src/main/java/gdsc/codereview/domain/lecture/dto/LectureDto.java
@@ -1,19 +1,32 @@
 package gdsc.codereview.domain.lecture.dto;
 
 import gdsc.codereview.domain.lecture.entity.Category;
+import gdsc.codereview.domain.lecture.entity.Lecture;
 import gdsc.codereview.domain.lecture.entity.Platform;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class LectureDto {
     private String title;
     private String summary;
-    //private String content;
     private Platform platform;
-    private Double lectureScore;
+    private Double score;
     private String thumbnail;
     private String link;
     private Category category;
+    private Long students;
+
+    // DTO -> Entity 변환 메서드
+    public Lecture toEntity() {
+        return Lecture.builder()
+                .title(title)
+                .thumbnail(thumbnail)
+                .summary(summary)
+                .score(score)
+                .students(students)
+                .lectureLink(link)
+                .build();
+    }
 }
--- src/main/java/gdsc/codereview/domain/lecture/entity/Lecture.java
+++ src/main/java/gdsc/codereview/domain/lecture/entity/Lecture.java
@@ -1,11 +1,11 @@
 package gdsc.codereview.domain.lecture.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Lecture {
 
     @Id
@@ -19,9 +19,7 @@ public class Lecture {
 
     private String summary; //간단한 소개
 
-    private String content; //전체 내용
-
-    private String platform;
+    private Platform platform;
 
     @Column(name = "lecture_score")
     private Double score;
@@ -29,5 +27,22 @@ public class Lecture {
     private String thumbnail; //이미지 경로 저장
 
     private String link; // 강좌 페이지
+
+    private Category category;
+
+    private Long students; //수강생 수
+
+    @Builder
+    public Lecture(String title, String thumbnail, Platform platform, Category category,
+                   String summary, Double score, Long students, String link) {
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.platform = platform;
+        this.category = category;
+        this.summary = summary;
+        this.score = score;
+        this.students = students;
+        this.link= link;
+    }
 
 }
