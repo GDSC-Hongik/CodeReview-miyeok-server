8d86b14aac690839e15f1414cb206ae33ea9dce6
ohseongbin <dannybin814@naver.com>
2025-02-12 20:24:48 +0900

    Feat: SeleniumConfig 구현

--- src/main/java/gdsc/codereview/domain/lecture/service/InflearnCrawlingService.java
+++ src/main/java/gdsc/codereview/domain/lecture/service/InflearnCrawlingService.java
@@ -28,7 +28,9 @@ public class InflearnCrawlingService {
 
     private final LectureRepository lectureRepository;
 
-    public void onePageLectureCrawling(WebDriver driver) throws Exception{ //한 페이지 크롤링
+    public void onePageLectureCrawling(String url, WebDriver driver) throws Exception{ //한 페이지 크롤링
+
+        driver.get(url);
 
         try{
             List<WebElement> ulElements = driver.findElements(By.xpath("//ul[contains(@class, 'mantine-1avyp1d')]"));
@@ -56,12 +58,7 @@ public class InflearnCrawlingService {
 
         System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("window-size=1920,1000");
-        options.addArguments("--headless=new");
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36");
-
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(SeleniumConfig.getChromeOptions());
 
 //        List<String> allLecturesLink = new ArrayList<>();
 
@@ -73,9 +70,7 @@ public class InflearnCrawlingService {
             //페이지네이션
             if(i!=1) url+="&page_number="+i;
 
-            driver.get(url);
-
-            onePageLectureCrawling(driver);
+            onePageLectureCrawling(url, driver);
 //            allLecturesLink.addAll(onePageLectureCrawling(driver));
         }
 
