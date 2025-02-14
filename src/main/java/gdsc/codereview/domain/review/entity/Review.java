d70ebf749b205ebb2ccea2a52b15b9758f5bf28a
ohseongbin <dannybin814@naver.com>
2025-02-14 14:24:50 +0900

    Feat: 리뷰 도메인 구현

---
+++ src/main/java/gdsc/codereview/domain/review/entity/Review.java
@@ -0,0 +1,45 @@
+package gdsc.codereview.domain.review.entity;
+
+
+import gdsc.codereview.global.Platform;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@RequiredArgsConstructor
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    // 유저 - 크롤링
+    private String username;
+
+    private String content; //리뷰 내용
+
+    @Enumerated(EnumType.STRING)
+    private Platform platform;
+
+    private Long score; //등록평점
+
+    private Long liked;
+
+    private Long hated;
+
+    @Builder
+    public Review(String username, String content, Platform platform,
+                  Long score, Long liked, Long hated) {
+        this.username = username;
+        this.content = content;
+        this.platform = platform;
+        this.score = score;
+        this.liked = liked;
+        this.hated = hated;
+    }
+}
