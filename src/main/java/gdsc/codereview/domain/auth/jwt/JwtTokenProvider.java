dc86605e9c26a4624936dabb93b4a0487f49a61a
서가영 <alstpgml0812@gmail.com>
2025-02-17 21:30:40 +0900

    Feat: jwt 토큰 처리

---
+++ src/main/java/gdsc/codereview/domain/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package gdsc.codereview.domain.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.access-token-validity}")
+    private long validityInMilliseconds;
+
+    // JWT 생성
+    public String createToken(String email) {
+        Claims claims = Jwts.claims().setSubject(email); // 사용자 정보 설정
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // JWT 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // HTTP 요청에서 JWT 토큰을 추출
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // "Bearer " 부분을 제거하고 토큰만 반환
+        }
+        return null;
+    }
+
+    // JWT에서 사용자명 추출
+    public String getUsername(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+}
