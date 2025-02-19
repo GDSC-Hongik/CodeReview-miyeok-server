4245db42de276e07cf63126abc4a5a0cda09ca1a
서가영 <alstpgml0812@gmail.com>
2025-02-19 17:56:28 +0900

    Fix: permit all

--- src/main/java/gdsc/codereview/global/config/SecurityConfig.java
+++ src/main/java/gdsc/codereview/global/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                         .requestMatchers( "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/v3/api-docs/**", "/swagger-ui/index.html#/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/course/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/review/**", "/api/auth/sign-out/**", "api/auth/recreate/**","/api/user/**", "/api/mypage/**").authenticated()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(exceptionFilter, JwtFilter.class);
