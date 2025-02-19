ed549d467591df63e28eea846b4cd2c9f9cc5649
서가영 <alstpgml0812@gmail.com>
2025-02-19 21:56:38 +0900

    Feat: 특정 유저가 리뷰를 작성한 강좌 목록 조회하는 컨트롤러

--- src/main/java/gdsc/codereview/domain/review/controller/ReviewController.java
+++ src/main/java/gdsc/codereview/domain/review/controller/ReviewController.java
@@ -1,11 +1,16 @@
 package gdsc.codereview.domain.review.controller;
 
+import gdsc.codereview.domain.lecture.dto.LectureDto;
+import gdsc.codereview.domain.lecture.entity.Lecture;
 import gdsc.codereview.domain.review.dto.ReviewCreateDto;
 import gdsc.codereview.domain.review.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +24,14 @@ public class ReviewController {
         reviewService.createReview(requestDto.getCourseId(), requestDto.getEmail(), requestDto.getContent(), requestDto.getScore());
     }
 
+    @GetMapping("/{userId}/mypage/list")
+    @Operation(summary = "특정 유저가 리뷰를 작성한 강좌 목록 조회")
+    public ResponseEntity<List<LectureDto>> getLecturesByUser(@PathVariable Long userId) {
+        List<Lecture> lectures = reviewService.getLecturesByUser(userId);
+        List<LectureDto> response = lectures.stream()
+                .map(LectureDto::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(response);
+    }
 
 }
