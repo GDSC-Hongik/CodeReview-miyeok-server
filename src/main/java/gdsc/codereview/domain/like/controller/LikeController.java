76495d2d4e2059c6f002d9d266823292cb676ebb
서가영 <alstpgml0812@gmail.com>
2025-02-20 01:12:43 +0900

    Feat: add swagger Operation

--- src/main/java/gdsc/codereview/domain/like/controller/LikeController.java
+++ src/main/java/gdsc/codereview/domain/like/controller/LikeController.java
@@ -6,6 +6,7 @@ import gdsc.codereview.domain.like.entity.LikeType;
 import gdsc.codereview.domain.like.service.LikeService;
 import gdsc.codereview.domain.review.entity.Review;
 import gdsc.codereview.domain.review.repository.ReviewRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class LikeController {
     private final LikeService likeService;
     private final ReviewRepository reviewRepository;
 
+    @Operation(summary = "좋아요/싫어요 토글", description = "같은 타입 2번 요청 시 취소됨. 취소하지 않은 채로 다른 타입 요청 시 오류 반환")
     @PostMapping("/{reviewId}")
     public LikeResponseDto toggleLike(
             @PathVariable Long reviewId,
@@ -28,6 +30,7 @@ public class LikeController {
         return likeService.toggleLike(userId, reviewId, likeType);
     }
 
+    @Operation(summary = "좋아요/싫어요 개수")
     @GetMapping("/{reviewId}")
     public ResponseEntity<CountResponseDto> getLikeDislikeCount(@PathVariable Long reviewId) {
         Review review = reviewRepository.findById(reviewId)
