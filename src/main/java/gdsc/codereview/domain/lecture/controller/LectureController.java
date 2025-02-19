315a2fa19b15d4655877535bacddae868baaf283
ohseongbin <dannybin814@naver.com>
2025-02-19 21:19:48 +0900

    Docs: Swagger API 문서  - @Operation 어노테이션 추가

--- src/main/java/gdsc/codereview/domain/lecture/controller/LectureController.java
+++ src/main/java/gdsc/codereview/domain/lecture/controller/LectureController.java
@@ -5,6 +5,7 @@ import gdsc.codereview.domain.lecture.dto.LectureReviewDto;
 import gdsc.codereview.domain.lecture.entity.Category;
 import gdsc.codereview.domain.lecture.entity.Lecture;
 import gdsc.codereview.domain.lecture.service.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,24 +21,28 @@ public class LectureController {
     private final LectureService lectureService;
 
     @GetMapping
+    @Operation(summary = "강좌 전체 리스트", description = "모든 강좌를 가져옵니다.")
     public ResponseEntity<List<LectureDto>> getLectureList(){
         List<LectureDto> lectureDtos = lectureService.getAllLectures();
         return ResponseEntity.ok(lectureDtos);
     }
 
     @GetMapping("/category")
+    @Operation(summary = "카테고리별 강좌 리스트", description = "카테고리를 선택했을때 특정 카테고리의 강좌를 가져옵니다.")
     public ResponseEntity<List<LectureDto>> getCatagoryLectureList(@RequestParam("category") Category category){
         List<LectureDto> lectureDtos = lectureService.getLecturesByCategory(category);
         return ResponseEntity.ok(lectureDtos);
     }
 
     @GetMapping("/course")
+    @Operation(summary = "특정 강좌", description = "특정 강좌 페이지를 가져옵니다(리뷰가 들어있는)")
     public ResponseEntity<List<LectureReviewDto>> getLectureWithReview(@RequestParam("title") String title){
         List<LectureReviewDto> lectureReviewDtos = lectureService.getLectureWithReview(title);
         return ResponseEntity.ok(lectureReviewDtos);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "검색", description = "제목이나 강사를 키워드로 검색할 수 있습니다.")
     public ResponseEntity<?> searchKeyword(@RequestParam("keyword") String keyword){
         List<LectureDto> lectureDtos = lectureService.searchLectureOrInstructor(keyword);
 
--- src/main/java/gdsc/codereview/domain/review/controller/ReviewController.java
+++ src/main/java/gdsc/codereview/domain/review/controller/ReviewController.java
@@ -2,6 +2,7 @@ package gdsc.codereview.domain.review.controller;
 
 import gdsc.codereview.domain.review.dto.ReviewCreateDto;
 import gdsc.codereview.domain.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/review/create")
+    @Operation(summary = "리뷰등록", description = "특정 강좌의 리뷰를 등록합니다(CODEREVIEW 사이트 내에서)")
     public void createReview(@RequestBody ReviewCreateDto requestDto){
         reviewService.createReview(requestDto.getCourseId(), requestDto.getEmail(), requestDto.getContent(), requestDto.getScore());
     }
